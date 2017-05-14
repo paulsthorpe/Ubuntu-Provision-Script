1ee4012ae1d8747ebf0831b434d8e0b0ae4ef4af
Paul Thorpe <paulsthorpe@gmail.com>
2017-05-14 16:03:30 -0400

    ditch nvm for stable 7.x node

--- provision.sh
+++ provision.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo '|========== Updating ==========|'
-sudo add-apt-repository ppa:webupd8team/atom
+sudo add-apt-repository -y ppa:webupd8team/atom
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo apt-get install curl
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y curl
 #VSCode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
@@ -24,28 +24,24 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update
 echo '|========== Installing Git ==========|'
 sudo apt-get install -y git
+git config --global user.name "Paul Thorpe"
+git config --global user.email paulsthorpe@gmail.com
 echo '|========== Installing Sublime ==========|'
 sudo apt-get install -y sublime-text-installer
 echo '|========== Installing Atom ==========|'
 sudo apt-get install -y atom
 echo '|========== Installing VLC Media Player ==========|'
 sudo apt-get install -y vlc
-echo '|========== Installing NVM ==========|'
-# download nvm
-git clone https://github.com/creationix/nvm.git ~/.nvm
-cd ~/.nvm
-git checkout `git describe --abbrev=0 --tags`
-# set variables
-export NVM_DIR="~/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-# update .bashrc
-cat << 'END' >> ~/.bashrc
-# nvm settings:
-export NVM_DIR="~/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-END
+echo '|========== Installing NodeJS ==========|'
+# download node
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 # install node/npm
-nvm install stable
+sudo apt-get install -y nodejs
+# fix node permissons
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
 # install npm packages
 echo '|========== Installing Gulp ==========|'
 npm install -g gulp
@@ -66,16 +62,16 @@ sudo chmod 755 /usr/local/bin/composer
 echo '|========== Installing Laravel Installer ==========|'
 composer global require "laravel/installer"
 echo '|========== Installing Spotify ==========|'
-sudo apt-get install spotify-client
+sudo apt-get install -y spotify-client
 echo '|========== Installing Gimp ==========|'
-sudo apt-get install gimp
+sudo apt-get install -y gimp
 echo '|========== Installing BleachBit ==========|'
 sudo apt-get install -y bleachbit
 echo '|========== Installing Minetest ==========|'
 sudo apt-get install -y minetest
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 echo '|========== Installing Googler ==========|'
 sudo curl -o /usr/local/bin/googler https://raw.githubusercontent.com/jarun/googler/v3.1/googler && sudo chmod +x /usr/local/bin/googler
 sudo googler -u
 echo '|========== Installing Unrar Free ==========|'
-sudo apt-get install unrar-free
+sudo apt-get install -y unrar-free
